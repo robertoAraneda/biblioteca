c501c7ab15ca6e5eb6335249069194190c48748b
robaraneda <48061781+robaraneda@users.noreply.github.com>
2019-04-20 19:45:03 -0400

    agregar tipo muestra

--- Siel/src/java/modelo/TipoMuestra.java
+++ Siel/src/java/modelo/TipoMuestra.java
@@ -2,5 +2,44 @@ package modelo;
 
 public class TipoMuestra {
 
+    private int idTipoMuestra;
+    private String descripcionTipoMuestra;
+    private int activo;
+
+    public TipoMuestra() {
+    }
+
+    public TipoMuestra(int idTipoMuestra, String descripcionTipoMuestra, int activo) {
+        this.idTipoMuestra = idTipoMuestra;
+        this.descripcionTipoMuestra = descripcionTipoMuestra;
+        this.activo = activo;
+    }
+
+    public int getIdTipoMuestra() {
+        return idTipoMuestra;
+    }
+
+    public void setIdTipoMuestra(int idTipoMuestra) {
+        this.idTipoMuestra = idTipoMuestra;
+    }
+
+    public String getDescripcionTipoMuestra() {
+        return descripcionTipoMuestra;
+    }
+
+    public void setDescripcionTipoMuestra(String descripcionTipoMuestra) {
+        this.descripcionTipoMuestra = descripcionTipoMuestra;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public void setActivo(int activo) {
+        this.activo = activo;
+    }
+    
+    //----------------------------------------------------------------métodos------------------------------------------------------------------------------------------
+    
     
 }
